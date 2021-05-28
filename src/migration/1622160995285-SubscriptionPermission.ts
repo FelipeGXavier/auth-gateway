8aab72f75ddb9cceacbec98fc27d1bad1f401b44
FelipeGXavier <felipexavier20015@gmail.com>
2021-05-27 21:33:49 -0300

    Add migrations

---
+++ src/migration/1622160995285-SubscriptionPermission.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class SubscriptionPermission1622160995285 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'subscription_permission',
+        columns: [
+          {
+            name: 'sub_id',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'permission_id',
+            type: 'int',
+            isPrimary: true,
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'fk_subp_sub_id',
+            columnNames: ['sub_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'subscription',
+          },
+          {
+            name: 'fk_subp_permission_id',
+            columnNames: ['permission_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'permission',
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('subscription_permission');
+  }
+}
